9b4815bc2d9188a7ed49d0746ca1b4bb050ac800
M0h4mmadH <mollataiefeh@gmail.com>
2021-12-03 14:17:33 +0330

    The word counter program code is now complete and tested with the given data successfully. Please note that if you want to test the code by yourself, you must first delete the output directory to avoid any further problems.

--- ex1-3.py
+++ ex1-3.py
@@ -5,3 +5,8 @@ spliter = lambda line: line.split(" ")
 mword = lambda word: (word, 1)
 counter = lambda a,b:a+b
 
+sc = SparkContext("local","word counter app")
+words = sc.textFile("./ShamsDaftar1.txt").flatMap(spliter)
+words_counter = words.map(mword).reduceByKey(counter)
+words_counter.saveAsTextFile("./output/")
+
